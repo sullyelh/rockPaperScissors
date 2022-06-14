9051e08a6ebb087982a47a908246f1b6891bcf7f
sullyelh <100114365+sullyelh@users.noreply.github.com>
2022-06-14 13:17:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function computerPlay() {
+options = ["rock", "paper", "scissors"]
+return random.choice(options)
+}
+
+function playRound(computerSelection, playerSelection) {
+let computerSelection = computerPlay()
+let result;
+if ((playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")) {
+    result = "You Win! " + playerSelection + " beats " + computerSelection +"!";
+    }
+else if (playerSelection == computerSelection) {
+    result = "It's a Tie, you both chose " + computerSelection +"!";
+    }
+else {
+    result = "You lose:( " + computerSelection + " beats "+ playerSelection+".";
+    }
+}
+
+function game {
+    for (i = 0, i > 5, i++)
+    playRound(computerSelection, playerSelection)
+}
