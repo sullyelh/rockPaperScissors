0bdd3d318e726fdce96f9c7bd822909e87f5c49b
sully <sully.elhassan@gmail.com>
2022-06-26 17:50:47 -0400

    Fixed computerPlay function

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-let playerSelection = prompt("Select rock, paper, or scissors");
+function playerPick(choice) {
+    let playerSelection;
+}
 
 function computerPlay() {
-options = ["rock", "paper", "scissors"]
-let computerSelection = random.choice(options);
-return computerSelection;
+    let options = ["rock", "paper", "scissors"];
+    const randomPick = Math.floor(Math.random() * options.length);
+    let computerSelection = options[randomPick];
+    return computerSelection;
 }
 
 function playRound(computerSelection, playerSelection) {
-let computerSelection = computerPlay();
-let result;
-
-if ((playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")) {
-    result = "You Win! " + playerSelection + " beats " + computerSelection +"!";
+    let result;
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        result = "You Win! " + playerSelection + " beats " + computerSelection +"!";
     }
-else if (playerSelection == computerSelection) {
-    result = "It's a Tie, you both chose " + computerSelection +"!";
+    else if (playerSelection == computerSelection) {
+        result = "It's a Tie, you both chose " + computerSelection +"!";
     }
-else {
-    result = "You lose:( " + computerSelection + " beats "+ playerSelection+".";
+    else {
+        result = "You lose:( " + computerSelection + " beats "+ playerSelection+".";
     }
 }
 
